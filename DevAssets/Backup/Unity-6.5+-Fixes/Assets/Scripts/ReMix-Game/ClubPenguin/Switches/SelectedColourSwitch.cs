2597f45314e809a21bbd7c0f327b3e7bed0a77d7
Malcolm <21669120+wednesday2024@users.noreply.github.com>
2026-08-21 04:43:32 -0500

    Backup

--- DevAssets/Backup/Unity-6.5+-Fixes/Assets/Scripts/ReMix-Game/ClubPenguin/Switches/SelectedColourSwitch.cs
+++ DevAssets/Backup/Unity-6.5+-Fixes/Assets/Scripts/ReMix-Game/ClubPenguin/Switches/SelectedColourSwitch.cs
@@ -1,4 +1,5 @@
 using ClubPenguin.Core;
+using Disney.MobileNetwork;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,48 @@ namespace ClubPenguin.Switches
 	{
 		[Tooltip("List of colours that will enable this switch. If empty matches anything")]
 		public AvatarColorDefinition[] Colours;
+
+		private AvatarDetailsData avatarDetailsData;
+
+		public void Start()
+		{
+			CPDataEntityCollection cPDataEntityCollection = Service.Get<CPDataEntityCollection>();
+			if (cPDataEntityCollection.TryGetComponent(cPDataEntityCollection.LocalPlayerHandle, out avatarDetailsData))
+			{
+				avatarDetailsData.PlayerColorChanged += onColorChanged;
+				onColorChanged(avatarDetailsData.BodyColor);
+			}
+		}
+
+		public void OnDestroy()
+		{
+			if (avatarDetailsData != null)
+			{
+				avatarDetailsData.PlayerColorChanged -= onColorChanged;
+			}
+		}
+
+		private void onColorChanged(Color color)
+		{
+			Change(IsMatch(color, Colours));
+		}
+
+		public static bool IsMatch(Color bodyColor, AvatarColorDefinition[] colours)
+		{
+			if (colours == null || colours.Length == 0)
+			{
+				return true;
+			}
+			for (int i = 0; i < colours.Length; i++)
+			{
+				Color color;
+				if (colours[i] != null && ColorUtility.TryParseHtmlString("#" + colours[i].Color, out color) && bodyColor == color)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
 
 		public override object GetSwitchParameters()
 		{
--- DevAssets/Backup/Unity-6.5+-Fixes/Assets/Scripts/ReMix-Game/LocomotionStateSwitch.cs
+++ DevAssets/Backup/Unity-6.5+-Fixes/Assets/Scripts/ReMix-Game/LocomotionStateSwitch.cs
@@ -1,10 +1,72 @@
 using ClubPenguin.Core;
+using ClubPenguin.Locomotion;
 using ClubPenguin.Net.Domain;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class LocomotionStateSwitch : Switch
 {
 	public List<LocomotionState> states = new List<LocomotionState>();
+
+	private LocomotionEventBroadcaster locoBroadcaster;
+
+	public void Start()
+	{
+		GameObject localPlayerGameObject = ClubPenguin.SceneRefs.ZoneLocalPlayerManager.LocalPlayerGameObject;
+		if (!localPlayerGameObject.IsDestroyed())
+		{
+			locoBroadcaster = localPlayerGameObject.GetComponent<LocomotionEventBroadcaster>();
+			if (locoBroadcaster != null)
+			{
+				locoBroadcaster.OnControllerChangedEvent += onControllerChanged;
+				Change(states.Contains(GetLocomotionState(localPlayerGameObject)));
+			}
+		}
+	}
+
+	public void OnDestroy()
+	{
+		if (locoBroadcaster != null)
+		{
+			locoBroadcaster.OnControllerChangedEvent -= onControllerChanged;
+		}
+	}
+
+	private void onControllerChanged(LocomotionController newController)
+	{
+		Change(states.Contains(FromController(newController)));
+	}
+
+	public static LocomotionState GetLocomotionState(GameObject penguin)
+	{
+		LocomotionTracker component = penguin.GetComponent<LocomotionTracker>();
+		if (component == null)
+		{
+			return LocomotionState.Default;
+		}
+		return FromController(component.GetCurrentController());
+	}
+
+	public static LocomotionState FromController(LocomotionController controller)
+	{
+		if (controller is RaceController)
+		{
+			return LocomotionState.Racing;
+		}
+		if (controller is SlideController)
+		{
+			return LocomotionState.Slide;
+		}
+		if (controller is SitController)
+		{
+			return LocomotionState.Sitting;
+		}
+		if (controller is ZiplineController)
+		{
+			return LocomotionState.Zipline;
+		}
+		return LocomotionState.Default;
+	}
 
 	public override object GetSwitchParameters()
 	{
