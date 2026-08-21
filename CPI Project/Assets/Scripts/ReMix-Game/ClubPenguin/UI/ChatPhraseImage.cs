30cc281cc661ed6c9ce4f64ee6d2469d5e600ff5
Malcolm <21669120+wednesday2024@users.noreply.github.com>
2026-08-20 19:22:15 -0500

    Update ChatPhraseImage.cs

--- CPI Project/Assets/Scripts/ReMix-Game/ClubPenguin/UI/ChatPhraseImage.cs
+++ CPI Project/Assets/Scripts/ReMix-Game/ClubPenguin/UI/ChatPhraseImage.cs
@@ -4,72 +4,126 @@ using UnityEngine.UI;
 
 namespace ClubPenguin.UI
 {
-	public class ChatPhraseImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler, IEventSystemHandler
-	{
-		[SerializeField]
-		private GameObject content;
+    public class ChatPhraseImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler, IEventSystemHandler
+    {
+        [SerializeField]
+        private GameObject content;
 
-		[SerializeField]
-		private float expandSize;
+        [SerializeField]
+        private float expandSize;
 
-		private RectTransform rectTransform;
+        private RectTransform rectTransform;
 
-		private float originalWidth;
+        private float originalWidth;
 
-		private float originalHeight;
+        private float originalHeight;
 
-		private void Awake()
-		{
-			base.gameObject.SetActive(false);
-		}
+        private void Awake()
+        {
+            base.gameObject.SetActive(false);
+        }
 
-		public void UpdatePosition()
-		{
-			base.gameObject.SetActive(true);
-			VerticalLayoutGroup componentInParent = GetComponentInParent<VerticalLayoutGroup>();
-			this.rectTransform = (base.transform as RectTransform);
-			RectTransform rectTransform = content.transform as RectTransform;
-			float num = originalWidth = rectTransform.rect.width + (float)componentInParent.padding.left + (float)componentInParent.padding.right;
-			originalHeight = this.rectTransform.rect.height;
-			scaleNormal();
-			updatePosition(originalWidth, originalHeight);
-		}
+        public void UpdatePosition()
+        {
+            if (this == null)
+            {
+                return;
+            }
 
-		public void OnPointerEnter(PointerEventData eventData)
-		{
-			scaleUp();
-		}
+            base.gameObject.SetActive(true);
 
-		public void OnSelect(BaseEventData eventData)
-		{
-			scaleUp();
-		}
+            VerticalLayoutGroup componentInParent = GetComponentInParent<VerticalLayoutGroup>();
+            this.rectTransform = base.transform as RectTransform;
 
-		public void OnPointerExit(PointerEventData eventData)
-		{
-			scaleNormal();
-		}
+            if (content == null || componentInParent == null || this.rectTransform == null)
+            {
+                return;
+            }
 
-		public void OnDeselect(BaseEventData eventData)
-		{
-			scaleNormal();
-		}
+            RectTransform rectTransform = content.transform as RectTransform;
 
-		private void scaleUp()
-		{
-			rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalWidth + expandSize);
-			rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, originalHeight + expandSize);
-		}
+            if (rectTransform == null)
+            {
+                return;
+            }
 
-		private void scaleNormal()
-		{
-			rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalWidth);
-			rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, originalHeight);
-		}
+            originalWidth = rectTransform.rect.width + componentInParent.padding.left + componentInParent.padding.right;
+            originalHeight = this.rectTransform.rect.height;
 
-		private void updatePosition(float width, float height)
-		{
-			rectTransform.anchoredPosition = new Vector3(width * 0.5f, (0f - height) * 0.5f, rectTransform.localPosition.z);
-		}
-	}
+            scaleNormal();
+            updatePosition(originalWidth, originalHeight);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (this == null)
+            {
+                return;
+            }
+
+            scaleUp();
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            if (this == null)
+            {
+                return;
+            }
+
+            scaleUp();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (this == null)
+            {
+                return;
+            }
+
+            scaleNormal();
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            if (this == null)
+            {
+                return;
+            }
+
+            scaleNormal();
+        }
+
+        private void scaleUp()
+        {
+            if (this == null || rectTransform == null)
+            {
+                return;
+            }
+
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalWidth + expandSize);
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, originalHeight + expandSize);
+        }
+
+        private void scaleNormal()
+        {
+            if (this == null || rectTransform == null)
+            {
+                return;
+            }
+
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalWidth);
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, originalHeight);
+        }
+
+        private void updatePosition(float width, float height)
+        {
+            if (this == null || rectTransform == null)
+            {
+                return;
+            }
+
+            rectTransform.anchoredPosition = new Vector3(width * 0.5f, (0f - height) * 0.5f, rectTransform.localPosition.z);
+        }
+    }
 }
