a79ccde601b04c197d253d5ccd446a3f9c543685
Malcolm <21669120+wednesday2024@users.noreply.github.com>
2026-08-22 14:20:54 -0500

    Fixes the igloo camera

--- DevAssets/Backup/Unity-6.5+-Fixes/Assets/Scripts/Cinematography/ClubPenguin/PinchZoomInput.cs
+++ DevAssets/Backup/Unity-6.5+-Fixes/Assets/Scripts/Cinematography/ClubPenguin/PinchZoomInput.cs
@@ -3,67 +3,101 @@ using Disney.LaunchPadFramework;
 using Disney.MobileNetwork;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace ClubPenguin
 {
-	[DisallowMultipleComponent]
-	internal class PinchZoomInput : MonoBehaviour
-	{
-		public float MouseSensitivity = 1f;
+    [DisallowMultipleComponent]
+    internal class PinchZoomInput : MonoBehaviour
+    {
+        public float MouseSensitivity = 1f;
 
-		public float TouchSensitivity = 2f;
+        public float TouchSensitivity = 2f;
 
-		public float PreviousZoom;
+        public float PreviousZoom;
 
-		private EventDispatcher dispatcher;
+        private EventDispatcher dispatcher;
 
-		private void Start()
-		{
-			dispatcher = Service.Get<EventDispatcher>();
-		}
+        private void Start()
+        {
+            dispatcher = Service.Get<EventDispatcher>();
+        }
 
-		private void Update()
-		{
-			float num = PreviousZoom;
+        private void Update()
+        {
+            float num = PreviousZoom;
+            Touchscreen touchscreen = Touchscreen.current;
 
-			// Use new Input System for touch and mouse input
-			if (Touchscreen.current != null && Touchscreen.current.touches.Count >= 2)
-			{
-				var touch0 = Touchscreen.current.touches[0];
-				var touch1 = Touchscreen.current.touches[1];
+            if (touchscreen != null)
+            {
+                TouchControl touch = null;
+                TouchControl touch2 = null;
+                int touchCount = 0;
 
-				var phase0 = touch0.phase.ReadValue();
-				var phase1 = touch1.phase.ReadValue();
+                for (int i = 0; i < touchscreen.touches.Count; i++)
+                {
+                    TouchControl currentTouch = touchscreen.touches[i];
 
-				if (phase0 == UnityEngine.InputSystem.TouchPhase.Moved && phase1 == UnityEngine.InputSystem.TouchPhase.Moved)
-				{
-					Vector2 position0 = touch0.position.ReadValue();
-					Vector2 deltaPosition0 = touch0.delta.ReadValue();
-					Vector2 position1 = touch1.position.ReadValue();
-					Vector2 deltaPosition1 = touch1.delta.ReadValue();
+                    if (currentTouch.press.isPressed)
+                    {
+                        if (touchCount == 0)
+                        {
+                            touch = currentTouch;
+                        }
+                        else if (touchCount == 1)
+                        {
+                            touch2 = currentTouch;
+                        }
 
-					Rect pixelRect = Camera.main.pixelRect;
-					if (pixelRect.Contains(position0) && pixelRect.Contains(position1))
-					{
-						float prevDist = (position0 - deltaPosition0 - (position1 - deltaPosition1)).magnitude;
-						float currDist = (position0 - position1).magnitude;
-						float delta = currDist - prevDist;
-						num += delta * TouchSensitivity / (float)Screen.width;
-					}
-				}
-			}
-			else if (Mouse.current != null)
-			{
-				float mouseScroll = Mouse.current.scroll.ReadValue().y;
-				num += mouseScroll * MouseSensitivity * 0.01f; // Mouse scroll is usually in 120 steps, scale it down
-			}
+                        touchCount++;
+                    }
+                }
 
-			num = Mathf.Clamp(num, 0f, 1f);
-			if (dispatcher != null && !Mathf.Approximately(num, PreviousZoom))
-			{
-				dispatcher.DispatchEvent(new InputEvents.ZoomEvent(num));
-				PreviousZoom = num;
-			}
-		}
-	}
+                if (touchCount == 2)
+                {
+                    if (touch.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Moved &&
+                        touch2.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Moved)
+                    {
+                        Vector2 position = touch.position.ReadValue();
+                        Vector2 deltaPosition = touch.delta.ReadValue();
+                        Vector2 position2 = touch2.position.ReadValue();
+                        Vector2 deltaPosition2 = touch2.delta.ReadValue();
+
+                        Rect pixelRect = Camera.main.pixelRect;
+
+                        if (pixelRect.Contains(position) && pixelRect.Contains(position2))
+                        {
+                            float magnitude = (position - position2).magnitude;
+                            float magnitude2 = (position - deltaPosition - (position2 - deltaPosition2)).magnitude;
+                            float num2 = magnitude - magnitude2;
+
+                            num -= num2 * TouchSensitivity / (float)Screen.width;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Mouse mouse = Mouse.current;
+
+                if (mouse != null)
+                {
+                    float scroll = mouse.scroll.ReadValue().y;
+
+                    if (scroll != 0f)
+                    {
+                        num += Mathf.Sign(scroll) * 0.1f * MouseSensitivity;
+                    }
+                }
+            }
+
+            num = Mathf.Clamp(num, 0f, 1f);
+
+            if (dispatcher != null && num != PreviousZoom)
+            {
+                dispatcher.DispatchEvent(new InputEvents.ZoomEvent(num));
+                PreviousZoom = num;
+            }
+        }
+    }
 }
