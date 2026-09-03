8a280aa2792c70ffda6c9e71c357c05ed4766754
Malcolm <21669120+wednesday2024@users.noreply.github.com>
2026-09-03 14:44:02 -0500

    Revert "Fixes."
    This reverts commit 0a60472bb4cbf1e3285a4fe908f80f67ae8fabc0.

--- CPI Project/Assets/Fabric/Editor/Fabric.Core.Editor/Fabric.Core.Editor.cs
+++ CPI Project/Assets/Fabric/Editor/Fabric.Core.Editor/Fabric.Core.Editor.cs
@@ -7483,6 +7483,8 @@ namespace Fabric
 				if (m_Data == null)
 				{
 					m_Data = ScriptableObject.CreateInstance<PersistentData>();
+					string path = AssetDatabase.GenerateUniqueAssetPath(projectDataFolder + "PersistentData.asset");
+					AssetDatabase.CreateAsset(m_Data, path);
 				}
 			}
 			return m_Data;
